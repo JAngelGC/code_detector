923c74f612f9462f2f007409ae24a668bae4a3e4
JAngelGC <gocaangel9@gmail.com>
2024-05-17 13:53:41 -0600

    Added some docstrings

--- app/utils/kgram.py
+++ app/utils/kgram.py
@@ -1,93 +1,101 @@
 from pythonparser.lexer import Token
 from pythonparser import source
 from typing import List
-from difflib import SequenceMatcher
 
-def generate_kgrams(tokens: List[Token], k: int):
-    kgrams = []
+def generate_kgrams(tokens: List[Token], k: int) -> List[List[str]]:
+    """
+    Given a list of tokens, it returns its k-grams
+
+    Input:
+        tokens: List of tokens
+        k: Size of the gram
+    
+    Returns:
+        kgrams: List ok kgrams
+    """
+    kgrams: List[List[str]] = []
     for i in range(0, len(tokens) - k + 1):
         kgram = [tokens[t].kind for t in range(i, i + k)]
         kgrams.append(kgram)
     
     return kgrams
 
-def custom_hash(s, base=256, prime=101):
+
+def calculate_hash(kgram: List[str], base: int = 256, prime: int = 101):
     """
+    Given kgram, it calculates a hash
+
+    Input:
+        kgram: List of tokens
+        base: Base 
+        prime: Prime number to be mod
+    
+    Returns:
+        hash_value: Hash of the kgram
+
+    TODO: Update this function, think of value for each token
     """
-    hash_value = 0
-    for gram in s:
-        for char in gram:
+    hash_value: int = 0
+    for token in kgram:
+        for char in token:
             hash_value = (hash_value * base + ord(char)) % prime
+
     return hash_value
 
 
-def hash_kgrams(kgrams):
-    hashed_kgrams = []
-    for kg in kgrams:
+def hash_kgrams(kgrams: List[List[str]]):
+    """
+    For kgrams, it returns a list of its hashes
 
-        # hashed_kgram = [custom_hash(k) for k in kg]
-        hashed_kgrams.append(custom_hash(kg))
+    Input:
+        kgrams: List of kgrams
+    
+    Returns:
+        hashed_grams: List of kgram hashes
+    """
+    hashed_kgrams: List[int] = []
+    for kg in kgrams:
+        hashed_kgrams.append(calculate_hash(kg))
 
     return hashed_kgrams
 
-def generate_windows(hashes, t, k):
-    w = t - k + 1
-    windows = []
+
+def generate_windows(hashes: List[str], t: int, k: int):
+    """
+    Creates a list of windows of size w
+
+    Input:
+        hashes: List of kgram hashes
+        t: Threshold
+        k: Size of kgrams
+    
+    Returns:
+        windows: List of the windows
+    """
+    w: int = t - k + 1
+    windows: List[List[int]] = []
     for i in range(0, len(hashes) - w + 1):
-        kgram = [hashes[t] for t in range(i, i + w)]
-        windows.append(kgram)
+        window: List[int] = [hashes[t] for t in range(i, i + w)]
+        windows.append(window)
     
     return windows
 
 
+def generate_fingerprint(windows: List[List[int]]):
+    """
+    Given windows of kgram hashes, it returns its fingerprint
 
-
-def generate_fingerprint(windows):
-    fp = []
+    Input:
+        windows: Windows of kgram hashes
+    
+    Returns:
+        fp: Fingerprint of the document
+    """
+    fp: List[int] = []
     for window in windows:
         fp.append(min(window))
     
     return fp
-
-
-
-
-# def rabin_karp_function(tokens, k, base=256, prime=101):
-#     n = len(tokens)
-#     m = k # len of the k-gram
-    
-#     window_hash = 0
-#     h = 1  # b^(m-1) % prime
-    
-#     # Precompute h = base^(m-1) % prime
-#     for i in range(m - 1):
-#         h = (h * base) % prime
-    
-#     results = []
-
-#     # Compute the initial hash values
-#     # print(f"len m: {m}")
-#     # print(tokens)
-#     for i in range(m):
-#         # print(tokens[i])
-#         # print(tokens[i])
-#         window_hash = (base * window_hash + custom_hash(tokens[i])) % prime
-#         results.append(window_hash)
-    
-#     for i in range(n - m + 1):
-#         print(tokens[i], " -- ", custom_hash(tokens[i]))
-#         # Calculate the hash for the next window
-#         if i < n - m:
-#             window_hash = (base * (window_hash - custom_hash(tokens[i]) * h) + custom_hash(tokens[i + m])) % prime
-#             results.append(window_hash)
-            
-#             # Handle negative hash values
-#             if window_hash < 0:
-#                 window_hash += prime
-    
-#     return results
-
-
 
 
 
